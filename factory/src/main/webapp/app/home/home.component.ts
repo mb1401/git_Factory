af461ab45c995d11f4a49d66f3415cca3449c1df
Unknown <mathias_moys@hotmail.com>
2018-06-18 16:46:11 +0200

    pageAccueil HTML

--- factory/src/main/webapp/app/home/home.component.ts
+++ factory/src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-
 import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
@@ -45,4 +44,5 @@ export class HomeComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
+
 }
