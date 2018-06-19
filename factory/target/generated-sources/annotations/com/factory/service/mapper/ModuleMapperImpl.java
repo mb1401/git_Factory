39394bdd0566422be948553f77ebb8caf7629775
Unknown <mbsopra@gmail.com>
2018-06-19 15:29:18 +0200

    authorities

--- factory/target/generated-sources/annotations/com/factory/service/mapper/ModuleMapperImpl.java
+++ factory/target/generated-sources/annotations/com/factory/service/mapper/ModuleMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-06-19T14:36:02+0200",
+    date = "2018-06-19T14:49:43+0200",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_171 (Oracle Corporation)"
 )
 @Component
--- factory/target/generated-sources/annotations/com/factory/service/mapper/SalleMapperImpl.java
+++ factory/target/generated-sources/annotations/com/factory/service/mapper/SalleMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-06-19T14:36:01+0200",
+    date = "2018-06-19T14:49:42+0200",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_171 (Oracle Corporation)"
 )
 @Component
