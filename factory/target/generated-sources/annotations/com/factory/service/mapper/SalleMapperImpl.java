162b938bc0cf6ff7d378fd5f6501a8e84ec53098
Unknown <mbsopra@gmail.com>
2018-06-19 10:36:18 +0200

    update

--- factory/target/generated-sources/annotations/com/factory/service/mapper/SalleMapperImpl.java
+++ factory/target/generated-sources/annotations/com/factory/service/mapper/SalleMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-06-18T17:50:51+0200",
+    date = "2018-06-19T10:26:52+0200",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_171 (Oracle Corporation)"
 )
 @Component
