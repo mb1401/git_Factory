4c5157f6e9f0b8a3c59ae094f73d42448ed86c5f
Unknown <mbsopra@gmail.com>
2018-06-21 10:58:25 +0200

    Auto stash before merge of "master" and "origin/master"

--- factory/src/main/webapp/app/home/home.component.ts
+++ factory/src/main/webapp/app/home/home.component.ts
@@ -15,7 +15,7 @@ import { Router, NavigationEnd } from '@angular/router';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
-    
+
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
